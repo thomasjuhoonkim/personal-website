296ad5686f3039d5d9b35226b9f9870d4f704178
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-09-02 21:48:17 -0400

    Setup HTTPS server

--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
 const fs = require("fs");
 const path = require("path");
+const http = require("http");
 const https = require("https");
 const express = require("express");
 
-const port = process.env.PORT || 3000;
-const hostname = "thomasjuhoonkim.me";
+const httpPort = process.env.PORT || 80;
+const httpsPort = process.env.PORT || 443;
+const hostname = process.env.HOST || "localhost";
 
 const buildPath = path.join(__dirname, "../client/build");
 
-const cert = fs.readFileSync("./server.crt");
-const ca = fs.readFileSync("./server.ca-bundle");
-const key = fs.readFileSync("./server.key");
+const cert = fs.readFileSync("./ssl/server.crt", "utf8");
+// const ca = fs.readFileSync("./ssl/server.ca-bundle", "utf8");
+const key = fs.readFileSync("./ssl/server.key", "utf8");
 const options = {
   cert: cert,
-  ca: ca,
+  // ca: ca,
   key: key,
 };
 
 const app = express();
 app.use(express.static(buildPath));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(buildPath, "index.html"));
 });
 
+// const httpServer = http.createServer(app);
+// httpServer.listen(httpPort, () => {
+//   console.log(`HTTP Server is running on port ${httpPort}!`);
+// });
+
 const httpsServer = https.createServer(options, app);
-httpsServer.listen(port, hostname);
+httpsServer.listen(httpsPort, () => {
+  console.log(`HTTPS Server is running on port ${httpsPort}!`);
+});
