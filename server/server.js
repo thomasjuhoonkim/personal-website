b943c699624374d1d19409b033900521b64a29a4
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-09-02 22:02:09 -0400

    Complete HTTPS protocol

--- server/server.js
+++ server/server.js
@@ -1,14 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const http = require("http");
 const https = require("https");
 const express = require("express");
-
-const httpPort = process.env.PORT || 80;
-const httpsPort = process.env.PORT || 443;
-const hostname = process.env.HOST || "localhost";
-
-const buildPath = path.join(__dirname, "../client/build");
 
 const cert = fs.readFileSync("./ssl/server.crt", "utf8");
 const ca = fs.readFileSync("./ssl/server.ca-bundle", "utf8");
@@ -20,6 +13,7 @@ const options = {
 };
 
 const app = express();
+const buildPath = path.join(__dirname, "../client/build");
 app.use(express.static(buildPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
@@ -31,11 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const httpServer = http.createServer(app);
-httpServer.listen(httpPort, () => {
-  console.log(`HTTP Server is running on port ${httpPort}!`);
-});
-
+const httpsPort = process.env.PORT || 443;
 const httpsServer = https.createServer(options, app);
 httpsServer.listen(httpsPort, () => {
   console.log(`HTTPS Server is running on port ${httpsPort}!`);
