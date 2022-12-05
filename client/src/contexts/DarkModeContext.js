26c0f7d635c1bea4df62995a747f8a322bf7ab81
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-12-05 13:26:29 -0500

    Add expiry to local storage theme

--- client/src/contexts/DarkModeContext.js
+++ client/src/contexts/DarkModeContext.js
@@ -1,12 +1,36 @@
 import React, { createContext, useState } from "react";
 import { getCookieConsentValue } from "react-cookie-consent";
 
+const setWithExpiry = (key, value, ttl) => {
+  const now = new Date();
+  const item = {
+    value: value,
+    expiry: now.getTime() + ttl,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+const getWithExpiry = (key) => {
+  const itemStr = localStorage.getItem(key);
+
+  if (!itemStr) return null;
+
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+
+  if (now.getTime() > item.expiry) {
+    localStorage.removeItem(key);
+    return null;
+  }
+  return item.value;
+};
+
 const DarkModeContext = createContext();
 
 function DarkModeProvider(props) {
   const [theme, setTheme] = useState(
     getCookieConsentValue()
-      ? localStorage.getItem("theme")
+      ? getWithExpiry("theme")
       : window.matchMedia("(prefers-color-scheme: light)").matches
       ? "light"
       : "dark"
@@ -14,15 +38,15 @@ function DarkModeProvider(props) {
 
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
-    if (getCookieConsentValue()) localStorage.setItem("theme", newTheme);
+    if (getCookieConsentValue()) {
+      setWithExpiry("theme", newTheme, 1 * 60 * 60 * 1000);
+    }
     setTheme(newTheme);
   };
 
   return (
     <div>
-      <DarkModeContext.Provider
-        value={{ theme, switchTheme }}
-      >
+      <DarkModeContext.Provider value={{ theme, switchTheme }}>
         {props.children}
       </DarkModeContext.Provider>
     </div>
