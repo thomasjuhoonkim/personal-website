817c12c8e4018221fe164e2c19b6675b02fc6051
Thomas Kim <thomasjuhoonkim@gmail.com>
2022-12-12 19:23:36 -0500

    Modify catch all for heroku from app.all to app.use

--- api/index.js
+++ api/index.js
@@ -81,7 +81,9 @@ app.use(
     },
   })
 );
-app.all("*", function (req, res, next) {
+// monitor wildcard for performance? this isn't strictly necessary
+// after all, who is purposefully going to thomasjuhoonkim-api.herokuapp.com
+app.use((req, res, next) => {
   const host = req.header("host");
   if (host.match(/.*herokuapp\..*/)) {
     res.redirect(301, "https://api.thomasjuhoonkim.me" + req.originalUrl);
